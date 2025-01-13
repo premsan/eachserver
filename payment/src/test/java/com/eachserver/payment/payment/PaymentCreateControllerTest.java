6e76206be59a12fe9440b6e2eae80a557284c9cb
admin <admin@premsan.com>
2025-01-14 06:54:12 +0800

    Initial test

--- payment/src/test/java/com/eachserver/payment/payment/PaymentCreateControllerTest.java
+++ payment/src/test/java/com/eachserver/payment/payment/PaymentCreateControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
@@ -17,6 +18,7 @@ class PaymentCreateControllerTest {
     @Autowired private MockMvc mockMvc;
 
     @Test
+    @WithMockUser(authorities = {"PAYMENT_PAYMENT_CREATE"})
     void getPaymentCreate() throws Exception {
 
         mockMvc.perform(get("/payment/payment-create"))
